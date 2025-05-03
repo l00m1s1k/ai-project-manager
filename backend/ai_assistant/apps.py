d7291ca943b7edf42251b44c501958fc5e1405b4
Andrew <andriyrudiy@gmail.com>
2025-05-03 16:01:15 +0300

    Fix AppConfig path for ai_assistant

--- backend/ai_assistant/apps.py
+++ backend/ai_assistant/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AiAssistantConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ai_assistant'
+    name = 'backend.ai_assistant'
