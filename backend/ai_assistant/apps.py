319632bbbd84c99bbcdcfb7761d18fbbae90eb84
Andrew <andriyrudiy@gmail.com>
2025-05-03 14:48:15 +0300

    Fix all module paths for backend structure

--- backend/ai_assistant/apps.py
+++ backend/ai_assistant/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AiAssistantConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ai_project_management.ai_assistant'
+    name = 'ai_assistant'
