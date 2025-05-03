5870034b6edf96f9889d3bcd25e15f92e3fb1d25
Andrew <andriyrudiy@gmail.com>
2025-05-03 15:13:07 +0300

    Fix paths for Render deploy

--- backend/ai_project_management/wsgi.py
+++ backend/ai_project_management/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for ai_project_management project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.ai_project_management.settings')
-
 application = get_wsgi_application()
