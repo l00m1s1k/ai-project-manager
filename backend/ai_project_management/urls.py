90c766ffef10b3497c988f416417507a0f3b17e9
Andrew <andriyrudiy@gmail.com>
2025-05-06 09:04:50 +0300

    yt

--- backend/ai_project_management/urls.py
+++ backend/ai_project_management/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from django.contrib import admin
 from backend.ai_assistant import views
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     # AUTH
@@ -25,6 +24,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.ai_assistant.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
